d3ef08b7db2691e99b509391f29ccf744ab280a1
Ksawex4 <ksawex17@gmail.com>
2025-05-26 02:23:02 +0200

    if all players die, the game quits

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -9,3 +9,6 @@ var Beans = 0
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
+	var scene = get_tree().get_current_scene()
+	if !scene.has_node("Fency") && !scene.has_node("Toasty") && !scene.has_node("PanLoduwka"):
+		get_tree().quit()
