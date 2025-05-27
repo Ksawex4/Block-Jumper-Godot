d76f0646d84df415edb41cf30cf7c34761864633
Ksawex4 <ksawex17@gmail.com>
2025-05-27 18:08:17 +0200

    main menu buttons now turn yellow when you hover over them

--- Scripts/main_menu_buttons.gd
+++ Scripts/main_menu_buttons.gd
@@ -4,7 +4,4 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_new_button_pressed() -> void:
-	call_deferred("changeScene", "res://Scenes/test_room.tscn")
-
-func changeScene(scene):
-	get_tree().change_scene_to_file(scene)
+	get_tree().change_scene_to_file("res://Scenes/test_room.tscn")
