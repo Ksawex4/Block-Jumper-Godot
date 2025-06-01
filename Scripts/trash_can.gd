82e0eb1d8edc46e0db0b5c3763b4d989796ea7be
Ksawex4 <ksawex17@gmail.com>
2025-06-01 14:09:33 +0200

    added infinite trash can

--- Scripts/trash_can.gd
+++ Scripts/trash_can.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 var isColliding := false
+@export var isInfinite := false
+
+func _ready() -> void:
+	if isInfinite:
+		$Sprite2D.texture = load("res://Textures/TrashCan/InfiniteTrashCan.png")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	isColliding = true
@@ -10,13 +15,14 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 func _process(delta: float) -> void:
 	if isColliding && Input.is_action_just_pressed("Interract"):
-		isColliding = false
 		spawnSpamGuy()
-		queue_free()
+		if !isInfinite:
+			isColliding = false
+			queue_free()
 
 func spawnSpamGuy():
 	var shouldSpawn = randf_range(0.0, 10.0)
 	if shouldSpawn < 2.0:
 		var spam = preload("res://Scenes/Objects/spamguys.tscn").instantiate()
-		spam.position = Vector2(position.x + 70, position.y)
+		spam.position = Vector2(position.x + 100, position.y)
 		get_tree().current_scene.add_child(spam)
