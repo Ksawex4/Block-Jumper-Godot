9b0ce62f54e4beedf5bb3139b2183bc0ab691e80
Ksawex4 <ksawex17@gmail.com>
2025-06-01 19:12:17 +0200

    added a savePoint (saves the game)

---
+++ Scripts/save_point.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var isColliding = false
+var save = false
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("Idle")
+
+func _process(delta: float) -> void:
+	if isColliding && Input.is_action_just_pressed("Interract"):
+		$AnimatedSprite2D.play("Save")
+	if save:
+		save = false
+		Global.saveGame(global_position)
+		$AudioStreamPlayer.play()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	isColliding = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	isColliding = false
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	save = true
+
+
+func _on_audio_stream_player_finished() -> void:
+	$AnimatedSprite2D.play("Idle")
