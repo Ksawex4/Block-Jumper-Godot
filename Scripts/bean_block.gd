47f35deec72c00dfa32e169fd372570bb3b48297
Ksawex4 <ksawex17@gmail.com>
2025-06-01 12:34:02 +0200

    BeanBlock is now animated again

--- Scripts/bean_block.gd
+++ Scripts/bean_block.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
-@export var Size = Vector2(1, 1)
-
 func _ready() -> void:
+	var scaleMultiplier = scale
+	scale = Vector2(1.0, 1.0)
 	$NinePatchRect/AnimationPlayer.play("BeanBlock")
+	$NinePatchRect/AnimationPlayer.get_animation("BeanBlock").loop = true
 	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
-	$NinePatchRect.size = Size
-	$CollisionShape2D.shape.size = Size
+	$NinePatchRect.size *= scaleMultiplier
+	$CollisionShape2D.shape.size *= scaleMultiplier
 	$NinePatchRect.anchor_left = 0
 	$NinePatchRect.anchor_right = 0
 	$NinePatchRect.anchor_top = 0
 	$NinePatchRect.anchor_bottom = 0
-	$NinePatchRect.position = -Size / 2
-	$CollisionShape2D.shape.size = Size
 	$CollisionShape2D.position = Vector2.ZERO
-	$NinePatchRect/AnimationPlayer.get_animation("BeanBlock").loop = true
+	$NinePatchRect.position = -$CollisionShape2D.shape.size / 2
+	$CollisionShape2D.disabled = false
