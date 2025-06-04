dd2f4041b3338ad84a45fd84ba7e6c0e5b1f59a3
Ksawex4 <ksawex17@gmail.com>
2025-06-04 21:03:42 +0200

    added 2 new blocks (GrassBlock and StoneBlock)

--- Scripts/bean_block.gd
+++ Scripts/bean_block.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
 @export var isVisible = true
+@export var Block = "BeanBlock"
 
 func _ready() -> void:
+	if Block == "GrassBlock":
+		$NinePatchRect.texture = load("res://Textures/Blocks/GrassBlock.png")
+		zeroPatchMargin()
+	if Block == "StoneBlock":
+		$NinePatchRect.texture = load("res://Textures/Blocks/StoneBlock.png")
+		zeroPatchMargin()
 	visible = isVisible
 	var scaleMultiplier = scale
 	scale = Vector2(1.0, 1.0)
-	$NinePatchRect/AnimationPlayer.play("BeanBlock")
-	$NinePatchRect/AnimationPlayer.get_animation("BeanBlock").loop = true
+	if $NinePatchRect/AnimationPlayer.has_animation(Block):
+		$NinePatchRect/AnimationPlayer.play(Block)
+		$NinePatchRect/AnimationPlayer.get_animation(Block).loop = true
 	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
 	$NinePatchRect.size *= scaleMultiplier
 	$CollisionShape2D.shape.size *= scaleMultiplier
@@ -17,3 +25,9 @@ func _ready() -> void:
 	$NinePatchRect.anchor_bottom = 0
 	$CollisionShape2D.position = Vector2.ZERO
 	$NinePatchRect.position = -$CollisionShape2D.shape.size / 2
+
+func zeroPatchMargin():
+	$NinePatchRect.patch_margin_bottom = 0
+	$NinePatchRect.patch_margin_left = 0
+	$NinePatchRect.patch_margin_top = 0
+	$NinePatchRect.patch_margin_right = 0
